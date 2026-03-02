2112a59e05a9a126aace5e011e09c9618dadb1fc
Michael Kreil <github@michael-kreil.de>
2026-03-02 19:32:38 +0100

    refactor: improve code formatting and structure in build scripts and templates

--- script/build.ts
+++ script/build.ts
@@ -21,7 +21,7 @@ if (process.argv[2] === 'dev') {
 	//writeFileSync('../docs/neu.md', neu, 'utf8');
 }
 
-function build(): { alt: string, neu: string, html: string } {
+function build(): { alt: string; neu: string; html: string } {
 	let content = readFileSync('../template/de.html', 'utf8');
 	let { alt, neu, html } = generateContent(content);
 
@@ -35,7 +35,7 @@ function build(): { alt: string, neu: string, html: string } {
 	html = blockReplace(template, '/*style*/', style);
 	return { alt, neu, html };
 
-	function generateContent(content: string): { alt: string, neu: string, html: string } {
+	function generateContent(content: string): { alt: string; neu: string; html: string } {
 		return {
 			alt: convert((pre, alt, neu, post) => `${pre}${clean(alt)}${post}`),
 			neu: convert((pre, alt, neu, post) => `${pre}${clean(neu)}${post}`),
@@ -54,20 +54,25 @@ function build(): { alt: string, neu: string, html: string } {
 
 				return html;
 			}),
-		}
+		};
 
 		function clean(text: string): string {
 			return text.replace(/^\$+|\$+/, '');
 		}
 
-		function convert(cb: (pre: string, alt: string, neu: string, post: string) => string): string {
-			return content.replace(/([a-z]*)\[(.*?)\/(.*?)\]([a-z,.]*)/gis, (_, pre: string, alt: string, neu: string, post: string) => {
-				pre = pre.trim();
-				alt = alt.trim();
-				neu = neu.trim();
-				post = post.trim();
-				return cb(pre, alt, neu, post);
-			});
+		function convert(
+			cb: (pre: string, alt: string, neu: string, post: string) => string,
+		): string {
+			return content.replace(
+				/([a-z]*)\[(.*?)\/(.*?)\]([a-z,.]*)/gis,
+				(_, pre: string, alt: string, neu: string, post: string) => {
+					pre = pre.trim();
+					alt = alt.trim();
+					neu = neu.trim();
+					post = post.trim();
+					return cb(pre, alt, neu, post);
+				},
+			);
 		}
 	}
 
@@ -75,7 +80,7 @@ function build(): { alt: string, neu: string, html: string } {
 		const parts = content.split(find);
 		if (parts.length !== 2) throw new Error(`"${find}" must be exactly once in the content`);
 		const indent = parts[0].match(/[ \t]*$/)![0];
-		replace = replace.replace(/\n/gm, `\n${indent}`)
+		replace = replace.replace(/\n/gm, `\n${indent}`);
 		return parts.join(replace);
 	}
 }
